45fbedb1691ed7c317ae08bc32f3d71125e96cd8
Seth Primack <sethprimack@Seths-MacBook-Pro.local>
2024-01-16 09:52:18 -0600

    Add slight modification to test case

--- test/db/seeds_test.rb
+++ test/db/seeds_test.rb
@@ -8,12 +8,14 @@ class SeedsTest < ActionDispatch::IntegrationTest
     stub_request(:get, 'https://rss.slashdot.org/Slashdot/slashdotMain')
       .to_return(status: 200, body: File.read('test/fixtures/slashdot_feed.xml'))
 
-    Article.destroy_all
-
     load Rails.root.join('db', 'seeds.rb')
 
     assert_equal 15, Article.count
-
     assert Article.all.all? { |article| article.title.present? }
+
+    # Testing idempotency
+    load Rails.root.join('db', 'seeds.rb')
+
+    assert_equal 15, Article.count
   end
 end
