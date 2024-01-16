050d923f32de42e2860f297e5ed477d47033d920
Seth Primack <sethprimack@Seths-MacBook-Pro.local>
2024-01-16 07:53:50 -0600

    Move scripts, add rspec gem

--- app/javascript/articles.js
+++ app/javascript/articles.js
@@ -11,4 +11,41 @@ function updateReadStatus(articleId, checked) {
     });
 }
 
+/**
+ * The following 3 functions are responsible for setting the url param showReadArticles
+ * This url param determines whether to filter read articles
+ */
+function toggleReadArticlesSwitch() {
+    var showReadArticlesParam = getParameterByName('showReadArticles');
+    var showReadArticlesValue = showReadArticlesParam === 'true' ? 'false' : 'true';
+
+    // Modify the URL with the updated parameter value
+    var currentUrl = window.location.href;
+    var updatedUrl = updateQueryStringParameter(currentUrl, 'showReadArticles', showReadArticlesValue);
+
+    // Redirect to the updated URL
+    window.location.href = updatedUrl;
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+        return uri + separator + key + "=" + value;
+    }
+}
+
 window.updateReadStatus = updateReadStatus;
+window.toggleReadArticlesSwitch = toggleReadArticlesSwitch;
